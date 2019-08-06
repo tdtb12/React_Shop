a91bbf93a32b5f5db2c4cbb95fc5c2a824503384
JIM <tdtb12@gmail.com>
2019-08-06 20:00:28 +0800

    Updates

---
+++ precache-manifest.263d7dcda705b109d043a9b16228975b.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1dba1919af1bfaf7df3c47c28d225aa5",
+    "url": "/React_Shop/index.html"
+  },
+  {
+    "revision": "92d32ea43e15b7b95ca4",
+    "url": "/React_Shop/static/css/2.57797527.chunk.css"
+  },
+  {
+    "revision": "0ce1a146748c4fdbe08e",
+    "url": "/React_Shop/static/css/main.2f26c70f.chunk.css"
+  },
+  {
+    "revision": "92d32ea43e15b7b95ca4",
+    "url": "/React_Shop/static/js/2.087f79db.chunk.js"
+  },
+  {
+    "revision": "0ce1a146748c4fdbe08e",
+    "url": "/React_Shop/static/js/main.c1f2c813.chunk.js"
+  },
+  {
+    "revision": "71ad0c80ff7b6b48fa54",
+    "url": "/React_Shop/static/js/runtime~main.5e25beb3.js"
+  }
+]);
